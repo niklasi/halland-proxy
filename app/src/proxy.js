4fa68d1a32e8ef45bfee8d6a72818c36bb9caa3c
Niklas Ingholt <niklas@ingholt.com>
2016-09-30 11:50:19 +0200

    Correct pipe handling.

--- app/src/proxy.js
+++ app/src/proxy.js
@@ -1,8 +1,16 @@
 const http = require('http')
 const url = require('url')
 const net = require('net')
+const through = require('through2')
 
 const noop = () => {}
+const createNoopStream = () => {
+  return through(function (chunk, enc, cb) {
+    this.push(chunk)
+    cb()
+  })
+}
+
 const transformHeaders = (headers) => {
   return Object.keys(headers).map(header => {
     return {key: header, value: headers[header]}
@@ -21,25 +29,13 @@ module.exports = ({ port = 8888, requestStart = noop, requestPipe = [], response
 
       let proxyRequest = http.request(options)
 
-      // const _writeRawReq = proxyRequest._writeRaw.bind(proxyRequest)
-      // proxyRequest._writeRaw = (data, encoding, callback) => {
-      //   // console.log(data.toString())
-      //   _writeRawReq(data, encoding, callback)
-      // }
-
-      // const _writeRaw = response.socket.write.bind(response.socket)
-      // response.socket.write = (data, encoding, callback) => {
-      //   // console.log(data.toString())
-      //   _writeRaw(data, encoding, callback)
-      // }
-
       proxyRequest.on('response', (proxyResponse) => {
-        responsePipe.forEach(pipe => {
-          proxyResponse.pipe(pipe(requestData))
-        })
-
         const headers = transformHeaders(proxyResponse.headers)
         headers.forEach(header => response.setHeader(header.key, header.value))
+
+        responsePipe.reduce((proxyResp, pipe) => {
+          return proxyResp.pipe(pipe(requestData, proxyResponse.headers) || createNoopStream())
+        }, proxyResponse).pipe(response)
 
         response.on('finish', () => {
           responseDone({
@@ -49,12 +45,6 @@ module.exports = ({ port = 8888, requestStart = noop, requestPipe = [], response
             statusMessage: proxyResponse.statusMessage
           })
         })
-
-        proxyResponse.pipe(response)
-      })
-
-      requestPipe.forEach(pipe => {
-        request.pipe(pipe())
       })
 
       const { hostname, method } = options
@@ -62,7 +52,10 @@ module.exports = ({ port = 8888, requestStart = noop, requestPipe = [], response
 
       const requestData = { id: options.path, hostname, method, headers, url: options.path }
       requestStart(requestData)
-      request.pipe(proxyRequest)
+
+      requestPipe.reduce((r, p) => {
+        return r.pipe(p() || createNoopStream())
+      }, request).pipe(proxyRequest)
     })
 
     proxy.on('connect', (req, cltSocket, head) => {
