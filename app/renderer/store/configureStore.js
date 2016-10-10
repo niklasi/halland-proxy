53e3a7640f8c7b4658112e44b9962974251d3bf4
Niklas Ingholt <niklas@ingholt.com>
2016-10-10 10:57:26 +0200

    Only use redux logger in development.

--- app/renderer/store/configureStore.js
+++ app/renderer/store/configureStore.js
@@ -1,13 +1,20 @@
 const { createStore, applyMiddleware } = require('redux')
-const thunkMiddleware = require('redux-thunk')
-const createLogger = require('redux-logger')
+const thunk = require('redux-thunk')
 const rootReducer = require('../reducers')
 
 module.exports = (preloadedState) => {
+  const middlewares = [thunk]
+
+  if (process.env.NODE_ENV === `development`) {
+    const createLogger = require(`redux-logger`)
+    const logger = createLogger()
+    middlewares.push(logger)
+  }
+
   const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunkMiddleware, createLogger())
+    applyMiddleware(...middlewares)
   )
 
   return store
