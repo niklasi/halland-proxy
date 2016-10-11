f77cfa9eefecf001969047136993826367196c0e
Niklas Ingholt <niklas@ingholt.com>
2016-10-11 09:52:13 +0200

    Change sidebar style.

--- app/renderer/containers/sidebar.js
+++ app/renderer/containers/sidebar.js
@@ -1,5 +1,14 @@
 const React = require('react')
+const { List, ListItem } = require('material-ui/List')
+const SettingsIcon = require('material-ui/svg-icons/action/settings').default
 
+const iconStyle = {
+  fill: '#FFF'
+}
+/* eslint-disable react/jsx-indent */
 module.exports = () => <nav className='sidebar-container'>
-                         ...
-                       </nav>
+                        <List>
+                          <ListItem primaryText='&nbsp;' leftIcon={<SettingsIcon style={iconStyle} />} />
+                        </List>
+                      </nav>
+/* eslint-enable react/jsx-indent */
