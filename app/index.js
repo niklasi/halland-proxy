d7c6ac3e7d1f47dcfb320389a6a438943722c0b3
Niklas Ingholt <niklas@ingholt.com>
2016-10-14 11:29:29 +0200

    Do not hard code window id when starting proxy.

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, globalShortcut} = require('electron')
+const { app, BrowserWindow, globalShortcut } = require('electron')
 const { resolve } = require('path')
 
 let win = null
@@ -14,8 +14,12 @@ app.on('ready', function () {
   win = new BrowserWindow({width: 640, height: 800, titleBarStyle: 'hidden-inset'})
   win.loadURL('file://' + resolve(__dirname, 'renderer/index.html'))
 
-  proxy = new BrowserWindow({width: 400, height: 400})
+  proxy = new BrowserWindow({width: 400, height: 400, show: false})
   proxy.loadURL('file://' + resolve(__dirname, 'proxy.html'))
+
+  proxy.on('ready-to-show', () => {
+    proxy.webContents.send('gui-window', win.id)
+  })
 
   globalShortcut.register('f12', function () {
     win.toggleDevTools()
