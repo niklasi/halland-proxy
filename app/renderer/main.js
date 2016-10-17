0daa103a986ad52702718c42201efb4f44427c66
Niklas Ingholt <niklas@ingholt.com>
2016-10-17 11:10:05 +0200

    Restructure.

--- app/renderer/main.js
+++ app/renderer/main.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const { render } = require('react-dom')
 const { Provider } = require('react-redux')
-const App = require('./containers/app')
+const App = require('./components/app')
 const configureStore = require('./store/configureStore')
 const injectTapEventPlugin = require('react-tap-event-plugin')
 const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default
