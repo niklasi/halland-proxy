7b6ea05ec3b1da6e59c34f0228482b9ba9d52782
Niklas Ingholt <niklas@ingholt.com>
2016-10-19 10:40:23 +0200

    Use redux-logger if not in production mode.

--- app/renderer/store/configureStore.js
+++ app/renderer/store/configureStore.js
@@ -5,7 +5,7 @@ const rootReducer = require('../reducers')
 module.exports = (preloadedState) => {
   const middlewares = [thunk]
 
-  if (process.env.NODE_ENV === `development`) {
+  if (process.env.NODE_ENV !== 'production') {
     const createLogger = require(`redux-logger`)
     const logger = createLogger()
     middlewares.push(logger)
