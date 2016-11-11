dc52898f44fd238dfa70dde01f2b9613ead1ea6b
Niklas Ingholt <niklas@ingholt.com>
2016-11-11 15:51:06 +0100

    Add test.

--- test/components/test-component-request-pane.js
+++ test/components/test-component-request-pane.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import tap from 'babel-tap'
+import { test } from 'babel-tap'
 import { shallow } from 'enzyme'
-import Requests from '../../app/renderer/components/requests/request-pane'
+import Request from '../../app/renderer/components/requests/request-pane'
+import Toolbar from '../../app/renderer/components/requests/request-pane-toolbar'
 
-tap.test('<Requests />', test => {
-  shallow(<Requests request={{host: 'aaa', method: 'GET', httpVersion: '1.1'}} />)
-  test.pass('this is ok')
-  test.end()
+const defaultRequest = {
+  id: '123',
+  host: 'www.ingholt.com',
+  method: 'GET',
+  httpVersion: '1.1'
+}
+
+function component (request = defaultRequest) {
+  return shallow(<Request request={request} />)
+}
+
+test('<RequestPane />', t => {
+  t.ok(component())
+  t.end()
+})
+
+test('render toolbar', t => {
+  const toolbar = component().find(Toolbar)
+  t.equal(1, toolbar.length)
+  t.same({requestId: '123'}, toolbar.props())
+  t.end()
 })
