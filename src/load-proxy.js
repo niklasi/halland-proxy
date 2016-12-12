924abb1be11c4c9196e640afa8aaefe4b4961b36
Niklas Ingholt <niklas@ingholt.com>
2016-12-12 11:26:00 +0100

    Install plugins.

--- src/load-proxy.js
+++ src/load-proxy.js
@@ -1,8 +1,7 @@
 import createProxy from './lib/proxy'
 import openDb from './db'
 import { load as loadConfig } from './lib/config'
-import loadPlugins from './plugins'
-import through from 'through2'
+import { loadPlugins, updatePlugins, syncPlugins } from './plugins'
 import { remote, ipcRenderer as ipc } from 'electron'
 import { START_PROXY, ADD_REQUEST, ADD_RESPONSE, HTTP_MESSAGE_DETAILS, SEND_HTTP_MESSAGE_DETAILS } from './constants/ipcMessages'
 import debugFactory from 'debug'
@@ -11,53 +10,62 @@ const debug = debugFactory('halland-proxy:proxy')
 
 debug('Load config...')
 const config = loadConfig()
+
+syncPlugins(config.plugins)
+
 debug('Open database...')
 const db = openDb({path: config.db.path, backingStore: config.db.backingStore})
 
+ipc.on('update-plugins', (evt, windowId) => {
+  debug('Update plugins...')
+  updatePlugins((err) => {
+    if (err) {
+      debug('Error when installing plugins...', err)
+    } else {
+      debug('Plugins installed successfully...')
+    }
+  })
+})
+
 let win
 ipc.on(START_PROXY, (evt, windowId) => {
-  debug('Prepare to start proxy...')
-  const plugins = loadPlugins(config)
-  debug('Loaded plugins...', plugins)
   win = remote.BrowserWindow.fromId(windowId)
+  startProxy()
+})
+
+function startProxy () {
+  debug('Prepare to start proxy...')
+  const plugins = loadPlugins(config.plugins)
+  debug('Loaded plugins...', plugins)
 
   const options = {
     port: config.port,
     requestSetup: plugins.requestSetup,
     requestStart: (request) => {
-      db.put(`${request.id}!request!meta`, request)
+      db.put(`${request.id}!request`, request)
       win.webContents.send(ADD_REQUEST, request)
     },
     requestPipe: plugins.requestPipe,
     responseHeaders: plugins.responseHeaders,
-    responsePipe: [
-      (request, responseHeaders) => {
-        let chunkIndex = 0
-        return through(function (chunk, enc, cb) {
-          db.put(`${request.id}!response!body!${chunkIndex}`, chunk)
-          chunkIndex += 1
-          this.push(chunk)
-          cb()
-        })
-      }].concat(plugins.responsePipe),
+    responsePipe: plugins.responsePipe,
     responseDone: (response) => {
-      db.put(`${response.id}!response!meta`, response)
+      db.put(`${response.id}!response`, response)
       delete response.body
       win.webContents.send(ADD_RESPONSE, response)
     }
   }
-  debug('Create proxy with options...', options)
 
+  debug('Create proxy with options...', options)
   createProxy(options, (err) => {
     if (err) throw err
     debug(`Proxy server started on port ${options.port}...`)
   })
-})
+}
 
 ipc.on(SEND_HTTP_MESSAGE_DETAILS, (e, requestId) => {
-  db.get(`${requestId}!request!meta`, (err, request) => {
+  db.get(`${requestId}!request`, (err, request) => {
     if (err) debug(err)
-    db.get(`${requestId}!response!meta`, (err, response) => {
+    db.get(`${requestId}!response`, (err, response) => {
       if (err) debug(err)
       debug('Send http message details...')
       win.webContents.send(HTTP_MESSAGE_DETAILS, { request, response })
--- src/plugins.js
+++ src/plugins.js
@@ -1,17 +1,34 @@
 import { homedir } from 'os'
 import { resolve, basename } from 'path'
+import { exec } from 'child_process'
+import { writeFileSync } from 'fs'
 import mkdirp from 'mkdirp'
+import debugFactory from 'debug'
+import shellEnv from 'shell-env'
 
-export default (config) => {
+const debug = debugFactory('halland-proxy:plugins')
+
+function getPluginDir () {
   const pluginDir = resolve(homedir(), '.halland-proxy-plugins')
-
   mkdirp.sync(pluginDir)
+  return pluginDir
+}
 
-  const paths = config.plugins.map(plugin => {
+export function syncPlugins (plugins = []) {
+  const packageJsonPath = resolve(getPluginDir(), 'package.json')
+  const packageJson = require(packageJsonPath)
+  plugins.forEach(p => (packageJson.dependencies[p] = 'latest'))
+
+  writeFileSync(packageJsonPath, JSON.stringify(packageJson))
+}
+
+export function loadPlugins (plugins = []) {
+  const pluginDir = getPluginDir()
+  const paths = plugins.map(plugin => {
     return resolve(pluginDir, 'node_modules', plugin.split('#')[0])
   })
 
-  const plugins = {
+  const pluginSetup = {
     requestSetup: [],
     requestPipe: [],
     responseHeaders: [],
@@ -24,15 +41,37 @@ export default (config) => {
     try {
       plugin = require(path)()
     } catch (err) {
-      console.log(`Failed to load plugin ${name}.`)
+      debug(`Failed to load plugin ${name}.`)
     }
     plugin = plugin || {}
-    plugins.requestSetup = [...plugins.requestSetups, ...plugin.requestSetup]
-    plugins.requestPipe = [...plugins.requestPipe, ...plugin.requestPipe]
-    plugins.responseHeaders = [...plugins.responseHeaders, ...plugin.responseHeaders]
-    plugins.responsePipe = [...plugins.responsePipe, ...plugin.responsePipe]
+    plugin.requestSetup = plugin.requestSetup || []
+    plugin.requestPipe = plugin.requestPipe || []
+    plugin.responseHeaders = plugin.responseHeaders || []
+    plugin.responsePipe = plugin.responsePipe || []
+
+    pluginSetup.requestSetup = [...pluginSetup.requestSetup, ...plugin.requestSetup]
+    pluginSetup.requestPipe = [...pluginSetup.requestPipe, ...plugin.requestPipe]
+    pluginSetup.responseHeaders = [...pluginSetup.responseHeaders, ...plugin.responseHeaders]
+    pluginSetup.responsePipe = [...pluginSetup.responsePipe, ...plugin.responsePipe]
   })
 
   return plugins
+}
+
+export function updatePlugins (cb) {
+  shellEnv().then(env => {
+    env.npm_config_runtime = 'electron'
+    env.npm_config_target = process.versions.electron
+    env.npm_config_disturl = 'https://atom.io/download/atom-shell'
+    exec('npm prune && npm install --production', {
+      cwd: getPluginDir(),
+      env
+    }, err => {
+      if (err) {
+        return cb(err)
+      }
+      cb(null)
+    })
+  }).catch(cb)
 }
 
